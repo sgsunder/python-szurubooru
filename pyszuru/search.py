8abf2f67567db118bb6472baf5391c70d104de5d
Shyam Sunder <sgsunder1@gmail.com>
2020-09-27 11:10:24 -0400

    Support search with progress bar (via tqdm)

--- pyszuru/search.py
+++ pyszuru/search.py
@@ -1,6 +1,9 @@
 from typing import List, Generator
 
 from collections import namedtuple
+from contextlib import nullcontext
+
+from tqdm import tqdm
 
 from pyszuru.api import API, FileToken
 from pyszuru.resource import Resource
@@ -12,30 +15,45 @@ SearchResult = namedtuple("SearchResult", ["post", "distance", "exact"], default
 
 
 def _search_generic(
-    api: API, search_query: str, transforming_class: type, page_size: int
+    api: API,
+    search_query: str,
+    transforming_class: type,
+    page_size: int,
+    show_progress_bar: bool = False,
 ) -> Generator[Resource, None, None]:
     offset = 0
     total = None
-    while True:
-        page = api._call(
-            "GET",
-            transforming_class._get_class_urlparts(),
-            urlquery={"offset": offset, "limit": page_size, "query": search_query},
-        )
-        for item in page["results"]:
-            yield transforming_class(api, item)
-        offset = offset + len(page["results"])
-        total = page["total"]
-        if offset >= total:
-            break
+    with (tqdm() if show_progress_bar else nullcontext()) as pbar:
+        while True:
+            page = api._call(
+                "GET",
+                transforming_class._get_class_urlparts(),
+                urlquery={"offset": offset, "limit": page_size, "query": search_query},
+            )
+            offset = offset + len(page["results"])
+            if page["total"] != total:
+                total = page["total"]
+                if show_progress_bar:
+                    pbar.total = total
+                    pbar.refresh()
+            for item in page["results"]:
+                if show_progress_bar:
+                    pbar.update()
+                yield transforming_class(api, item)
+            if offset >= total:
+                break
 
 
-def search_tag(api: API, search_query: str, page_size: int = 20) -> Generator[Tag, None, None]:
-    return _search_generic(api, search_query, Tag, page_size)
+def search_tag(
+    api: API, search_query: str, page_size: int = 20, show_progress_bar: bool = False
+) -> Generator[Tag, None, None]:
+    return _search_generic(api, search_query, Tag, page_size, show_progress_bar)
 
 
-def search_post(api: API, search_query: str, page_size: int = 20) -> Generator[Post, None, None]:
-    return _search_generic(api, search_query, Post, page_size)
+def search_post(
+    api: API, search_query: str, page_size: int = 20, show_progress_bar: bool = False
+) -> Generator[Post, None, None]:
+    return _search_generic(api, search_query, Post, page_size, show_progress_bar)
 
 
 def search_by_image(api: API, image: FileToken) -> List[SearchResult]:
